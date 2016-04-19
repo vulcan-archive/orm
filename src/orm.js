76f8c80ec436fdc0e814bf278b89027b73e36e15
killswitch <josh+killswitch+github@joshmanders.com>
2016-04-19 15:54:17 -0500

    add connection pool to config

--- src/orm.js
+++ src/orm.js
@@ -5,13 +5,13 @@ import { MissingConnectionError } from './errors'
 const driverMappings = { sqlite: 'sqlite3', postgres: 'pg', mysql: 'mysql' }
 
 export default class ORM {
-  constructor ({ driver = 'sqlite', connection, ...options } = {}) {
+  constructor ({ driver = 'sqlite', connection, pool, ...options } = {}) {
     if (!connection) {
       throw new MissingConnectionError()
     }
     this.$options = defaultsDeep({}, options, { convertCase: false, useNullAsDefault: true })
     this.$client = driverMappings[driver]
-    this.$db = knex(defaultsDeep({}, { connection }, {
+    this.$db = knex(defaultsDeep({}, { connection, pool }, {
       client: this.$client,
       useNullAsDefault: this.$options.useNullAsDefault
     }))
